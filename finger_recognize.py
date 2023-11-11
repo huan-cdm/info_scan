5a556a822d6ea79b184175855d08895fb5be7e7f
huan-cdm <huan151009@163.com>
2023-11-11 22:14:25 +0800

    指纹显示优化

--- finger_recognize.py
+++ finger_recognize.py
@@ -13,6 +13,11 @@ def finger_scan(ip1):
     finger_list = []
     for i in result:
         result = os.popen('bash ./finger.sh finger'+''+' '+i).read()
-        finger_list.append(result)
+        result1 = result.replace("[1;31m","")
+        result2 = result1.replace("[0m","")
+        result3 = result2.replace("[1;32m","")
+        result4 = result3.replace("|",",")
+        result5 = result4.replace("Banner: ","")
+        finger_list.append(result5)
     
     return finger_list
