a3e3a630bf4d67cfd800a3d7afcfe2bc00ec0f95
huan-cdm <huan151009@163.com>
2023-11-25 17:26:02 +0800

    修改配置

--- gaodeapi.py
+++ gaodeapi.py
@@ -6,13 +6,11 @@ Date:[2023/11/25]
 import requests
 import json
 from config import gaodekey
-# API密钥  
-key = '2c0f28df8ad2748d51f49d8a075c6c88'  
 
 def gaodescan(keyvalue):
 
     
-    url = "https://restapi.amap.com/v3/place/text?keywords="+keyvalue+"&offset=20&page=1&key="+key+"&extensions=all"
+    url = "https://restapi.amap.com/v3/place/text?keywords="+keyvalue+"&offset=20&page=1&key="+gaodekey+"&extensions=all"
     hearder={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
         }
