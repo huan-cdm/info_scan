d72b659736f131ebfd19c034cbb69c6e9992fd14
huan-cdm <huan151009@163.com>
2024-07-16 20:38:30 +0800

    update server_check.sh

--- server_check.sh
+++ server_check.sh
@@ -1,6 +1,11 @@
  #! /bin/bash
  if [ "$1" == "-h" ]; then  
 	echo -e "[+]------------------------------------------"
+    echo "开启所有服务：bash server_check.sh start"  
+    echo "关闭所有服务：bash server_check.sh stop" 
+	echo "重启所有服务：bash server_check.sh restart" 
+	echo "查看服务状态：bash server_check.sh status" 
+	echo -e "[+]------------------------------------------"
     echo "开启infoscan：bash server_check.sh startinfoscan"  
     echo "关闭infoscan：bash server_check.sh stopinfoscan"  
 	echo "重启infoscan：bash server_check.sh restartinfoscan"  
@@ -21,8 +26,6 @@
 	echo "开启afrog报告：bash server_check.sh startafrogreport"
 	echo "关闭afrog报告：bash server_check.sh stopafrogreport"
 	echo "重启afrog报告：bash server_check.sh restartafrogreport"
-	echo -e "[+]------------------------------------------"
-	echo "查看服务状态：bash server_check.sh status"
 	echo -e "[+]------------------------------------------"
     exit 0  #退出脚本，如果不需要执行其他命令的话
 fi  
@@ -418,6 +421,177 @@ case "${1}" in
 	else
 		echo -e "URLFinderreport：" "\033[31mX\033[0m"
 	fi
+	;;
+
+
+	# 开启所有服务
+	start)
+	# 开启info_scan
+	nohup python3 ./scan_main_web.py > /dev/null 2>&1 &
+    sleep 0.5s
+	infopid=`ps -aux | grep  scan_main_web.py |awk -F " " '{print $2}' | wc -l`
+	#infoscan运行状态
+	if (( $infopid > 1 ))
+	then
+		echo -e "infoscan：" "\033[32m√\033[0m" 
+	else
+		echo -e "infoscan：" "\033[31mX\033[0m"
+	fi
+
+	# 开启目录扫描
+	nohup python3 ./dirscanmain.py > /dev/null 2>&1 &
+    sleep 0.5s
+	dirscanpid=`ps -aux | grep dirscanmain.py |awk -F " " '{print $2}' | wc -l`
+	#目录扫描服务
+	if (( $dirscanpid > 1 ))
+	then
+		echo -e "dirscan：" "\033[32m√\033[0m" 
+	else
+		echo -e "dirscan：" "\033[31mX\033[0m"
+	fi
+
+
+	# 开启xray报告
+	cd /TIP/batch_scan_domain/report
+    nohup python3 -m http.server 8081 --bind 127.0.0.1 > /dev/null 2>&1 &
+	sleep 0.5s
+	xraypid=`ps -aux | grep 8081 |awk -F " " '{print $2}' | wc -l`
+	#xray报告服务
+	if (( $xraypid > 1 ))
+	then
+		echo -e "xrayreport：" "\033[32m√\033[0m" 
+	else
+		echo -e "xrayreport：" "\033[31mX\033[0m"
+	fi
+
+	# 开启afrog
+	cd /TIP/info_scan/afrog_scan/reports
+    nohup python3 -m http.server 8082 --bind 127.0.0.1 > /dev/null 2>&1 &
+	
+	sleep 0.5s
+	afrogpid=`ps -aux | grep 8082 |awk -F " " '{print $2}' | wc -l`
+	# afrog服务状态
+	if (( $afrogpid > 1 ))
+	then
+		echo -e "afrogreport：" "\033[32m√\033[0m" 
+	else
+		echo -e "afrogreport：" "\033[31mX\033[0m"
+	fi
+
+	# 开启urlfinder
+	cd /TIP/info_scan/urlfinder_server/report
+    nohup python3 -m http.server 8089 --bind 127.0.0.1 > /dev/null 2>&1 &
+	
+	sleep 0.5s
+	urlfinderpid=`ps -aux | grep 8089 |awk -F " " '{print $2}' | wc -l`
+	#链接扫描报告服务
+	if (( $urlfinderpid > 1 ))
+	then
+		echo -e "urlfinderreport：" "\033[32m√\033[0m" 
+	else
+		echo -e "urlfinderreport：" "\033[31mX\033[0m"
+	fi
+	;;
+
+
+	# 关闭所有服务
+	stop)
+	# 关闭info_scan
+	pides=`ps -aux | grep  scan_main_web.py | awk -F " " '{print $2}'`
+	for aes in ${pides}
+	do
+		kill -9 ${aes} 2>/dev/null
+	done
+	sleep 0.5s
+	infopid=`ps -aux | grep  scan_main_web.py |awk -F " " '{print $2}' | wc -l`
+	#infoscan运行状态
+	if (( $infopid > 1 ))
+	then
+		echo -e "infoscan：" "\033[32m√\033[0m" 
+	else
+		echo -e "infoscan：" "\033[31mX\033[0m"
+	fi
+
+	# 关闭dirscan
+	dirscanpid=`ps -aux | grep  dirscanmain.py | awk -F " " '{print $2}'`
+	for dirid in ${dirscanpid}
+	do
+		kill -9 ${dirid} 2>/dev/null
+	done
+	sleep 0.5s
+    dirscanpid=`ps -aux | grep dirscanmain.py |awk -F " " '{print $2}' | wc -l`
+	#目录扫描服务
+	if (( $dirscanpid > 1 ))
+	then
+		echo -e "dirscan：" "\033[32m√\033[0m" 
+	else
+		echo -e "dirscan：" "\033[31mX\033[0m"
+	fi
+
+	# 关闭xray
+	pidd=`ps -aux | grep 8081 |awk -F " " '{print $2}'`
+    
+    for ii in ${pidd}
+	do
+		kill -9 ${ii} 2>/dev/null
+	done
+
+	sleep 0.5s
+	xraypid=`ps -aux | grep 8081 |awk -F " " '{print $2}' | wc -l`
+	#xray报告服务
+	if (( $xraypid > 1 ))
+	then
+		echo -e "xrayreport：" "\033[32m√\033[0m" 
+	else
+		echo -e "xrayreport：" "\033[31mX\033[0m"
+	fi
+
+	# 关闭afrog
+	afpid=`ps -aux | grep 8082 |awk -F " " '{print $2}'`
+    
+    for ii in ${afpid}
+	do
+
+		kill -9 ${ii} 2>/dev/null
+		
+	done
+
+	sleep 0.5s
+	afrogpid=`ps -aux | grep 8082 |awk -F " " '{print $2}' | wc -l`
+	# afrog服务状态
+	if (( $afrogpid > 1 ))
+	then
+		echo -e "afrogreport：" "\033[32m√\033[0m" 
+	else
+		echo -e "afrogreport：" "\033[31mX\033[0m"
+	fi
+
+
+	# 关闭urlfinder
+	linkpidd=`ps -aux | grep 8089 |awk -F " " '{print $2}'`
+    
+    for ii in ${linkpidd}
+	do
+		
+		kill -9 ${ii} 2>/dev/null
+		
+	done
+
+	sleep 0.5s
+	urlfinderpid=`ps -aux | grep 8089 |awk -F " " '{print $2}' | wc -l`
+	#链接扫描报告服务
+	if (( $urlfinderpid > 1 ))
+	then
+		echo -e "urlfinderreport：" "\033[32m√\033[0m" 
+	else
+		echo -e "urlfinderreport：" "\033[31mX\033[0m"
+	fi
+	;;
+
+	# 重启所有服务
+	restart)
+	bash server_check.sh stop
+	bash server_check.sh start
 	;;
 
 
