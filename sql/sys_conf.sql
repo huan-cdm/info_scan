52204f1f3e0e34101d6773d5e7c7ea12da75e5dc
huan-cdm <huan151009@163.com>
2025-05-07 17:15:03 +0800

    update sql

--- sql/sys_conf.sql
+++ sql/sys_conf.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80037
  File Encoding         : 65001
 
- Date: 07/05/2025 16:33:49
+ Date: 07/05/2025 17:13:45
 */
 
 SET NAMES utf8mb4;
@@ -28,5 +28,15 @@ CREATE TABLE `sys_conf`  (
   `fofa_key` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_conf
+-- ----------------------------
+INSERT INTO `sys_conf` VALUES (1, '60', NULL, NULL);
+INSERT INTO `sys_conf` VALUES (2, NULL, 'test@qq.com', '1231426ed6a3f3b84518312e12388b6b53012');
+INSERT INTO `sys_conf` VALUES (3, '1235wdqqD7mCbWQdehFqWhk5aKVK0OtwR0Z12', NULL, '');
+INSERT INTO `sys_conf` VALUES (4, '1232c0f28df8ad2748d51f49d8a075c6c8812', NULL, '');
+INSERT INTO `sys_conf` VALUES (5, '1231c9ca5b4d4eebd74f3b77675c919005b12', NULL, '');
+INSERT INTO `sys_conf` VALUES (6, 'test.ceye.io', NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
